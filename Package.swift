18fc9d90f74d38d2252cfbd95b76effe2b40c856
Ira Rosen <IRAR@il.ibm.com>
2016-04-14 13:05:07 +0300

    Version update

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Session",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 9),
     ]
 )
