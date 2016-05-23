426873111ad9914e9b3ccaa416a7c146f0799600
Ira Rosen <IRAR@il.ibm.com>
2016-05-23 09:47:38 +0300

    IBM-Swift/Kitura#446 Made two different constant salts for key derivation. Removed a call to StringUtils.

--- Sources/KituraSession/CookieCryptography.swift
+++ Sources/KituraSession/CookieCryptography.swift
@@ -16,7 +16,6 @@
 
 
 import Cryptor
-import KituraSys
 import LoggerAPI
 
 import Foundation
@@ -29,34 +28,24 @@ class CookieCryptography {
     ///
     /// Key for encryption (AES-128)
     ///
-    private var encryptonKey : [UInt8]
+    private var encryptionKey : [UInt8]
+    
     ///
     /// Key for signature (HMAC-SHA-256)
     ///
     private var signatureKey : [UInt8]
+    
     ///
     /// Length of cookie value before padding
     ///
     private let originalLength = 36
     
     init (secret: String) {
-        var salt : [UInt8]
-        do {
-            salt = try Random.generate(byteCount: 16)
-        }
-        catch {
-            salt = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-        }
+        let encryptionKeySalt = "If two witches would watch two watches, which witch would watch which watch?"
+        let signatureKeySalt = "Six sick hicks nick six slick bricks with picks and sticks."
         let keyLength = UInt(Cryptor.Algorithm.aes.defaultKeySize)
-        self.encryptonKey = PBKDF.deriveKey(fromPassword: secret, salt: salt, prf: .sha256, rounds: 2, derivedKeyLength: keyLength)
-        
-        do {
-            salt = try Random.generate(byteCount: 16)
-        }
-        catch {
-            salt = [15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
-        }
-        self.signatureKey = PBKDF.deriveKey(fromPassword: secret, salt: salt, prf: .sha256, rounds: 2, derivedKeyLength: keyLength)
+        self.encryptionKey = PBKDF.deriveKey(fromPassword: secret, salt: encryptionKeySalt, prf: .sha256, rounds: 2, derivedKeyLength: keyLength)
+        self.signatureKey = PBKDF.deriveKey(fromPassword: secret, salt: signatureKeySalt, prf: .sha256, rounds: 2, derivedKeyLength: keyLength)
     }
     
     ///
@@ -80,7 +69,7 @@ class CookieCryptography {
             dataToCipher = CryptoUtils.zeroPad(byteArray: plainData, blockSize: Cryptor.Algorithm.aes.blockSize)
         }
 
-        guard let cipherData = Cryptor(operation: .encrypt, algorithm: .aes, options: .none, key: encryptonKey, iv: iv).update(byteArray: dataToCipher)?.final() else {
+        guard let cipherData = Cryptor(operation: .encrypt, algorithm: .aes, options: .none, key: encryptionKey, iv: iv).update(byteArray: dataToCipher)?.final() else {
             Log.error("Failed to encrypt cookie")
             return nil
         }
@@ -127,7 +116,7 @@ class CookieCryptography {
         }
         
         // Decryption
-        guard let decryptedData = Cryptor(operation: .decrypt, algorithm: .aes, options: .none, key: encryptonKey, iv: iv).update(byteArray: cipherData)?.final() else {
+        guard let decryptedData = Cryptor(operation: .decrypt, algorithm: .aes, options: .none, key: encryptionKey, iv: iv).update(byteArray: cipherData)?.final() else {
             Log.error("Failed to decrypt cookie")
             return nil
         }
@@ -135,7 +124,7 @@ class CookieCryptography {
         var resultData = decryptedData
         // Remove padding
         resultData.removeSubrange(originalLength ..< decryptedData.count)
-        return StringUtils.fromUtf8String(CryptoUtils.data(from: resultData))!
+        return String(data: CryptoUtils.data(from: resultData), encoding: NSUTF8StringEncoding)
     }
     
 }
