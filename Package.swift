7db0e326ece021a9783f797ddec85cdef6333fb3
Mitesh Soni <mitesh_soni@rocketmail.com>
2018-01-02 17:55:49 +0530

    Added KituraSessionRedis to products

--- Package.swift
+++ Package.swift
@@ -26,6 +26,10 @@ let package = Package(
         .library(
             name: "KituraSession",
             targets: ["KituraSession"]
+        ),
+        .library(
+            name: "KituraSessionRedis",
+            targets: ["KituraSessionRedis"]
         )
     ],
     dependencies: [
@@ -33,13 +37,7 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "0.8.0"),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: "17.0.0"),
     ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "KituraSessionRedis",
-            targets: ["KituraSessionRedis"]
-        )
-    ],
+    
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
