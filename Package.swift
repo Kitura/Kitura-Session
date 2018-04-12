b3fdb72805f57a10b02ce536e823320756b2832b
Kye-Maloy <kye.maloy@ibm.com>
2018-04-12 14:33:51 +0100

    Updated to Blue Cryptor 1.0.0

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.1.0"),
-        .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", .upToNextMinor(from: "0.8.0")),
+        .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", .upToNextMinor(from: "1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
