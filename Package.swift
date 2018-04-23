21fcc772d5c9622bacb2bfffe95c984e83a57763
andy <andrew.lees11@ibm.com>
2018-04-23 13:16:17 +0100

    pointed package at private repo

--- Package.swift
+++ Package.swift
@@ -29,7 +29,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.3.0"),
+        .package(url: "https://github.ibm.com/Andrew-Lees11/Kitura.git", .branch("typeSafeMiddleware")),
+        //.package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.3.0"),
         .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.0"),
     ],
     targets: [
