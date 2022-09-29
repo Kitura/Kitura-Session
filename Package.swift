80dbf1b1e933e6f8a6958801eae06abe26cf5a99
Danny Sung <danny_sung@intuit.com>
2022-09-29 16:16:32 -0700

    Update Package.swift for 5.2 requirements

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Kitura/Kitura.git", from: "3.0.0"),
-        .package(url: "https://github.com/Kitura/BlueCryptor.git", from: "2.0.2"),
+        .package(name: "Cryptor", url: "https://github.com/Kitura/BlueCryptor.git", from: "2.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
